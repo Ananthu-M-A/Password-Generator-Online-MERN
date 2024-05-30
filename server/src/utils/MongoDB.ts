ab1027194afd8c804423c4960144f6d6f0dc53de
Ananthu M A <heartchaserananthu@gmail.com>
2024-05-30 21:08:23 +0530

    Starting new project - Password Generator

---
+++ server/src/utils/MongoDB.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import 'dotenv/config';
+
+export const connectDb = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+        console.log("Database is connected");
+    } catch (error) {
+        console.log("Error connecting database:", error);
+    }
+}
